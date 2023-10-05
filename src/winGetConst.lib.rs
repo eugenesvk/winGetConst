71d73397a52f0a94830abe54898e2df2e47a7a82
eugenesvk <eugenesvk@users.noreply.github.com>
2023-10-06 02:59:50 +0700

    rename dummy, bin/lib

--- src/winGetConst.lib.rs
+++ src/winGetConst.lib.rs
@@ -1,4 +1,4 @@
-#![allow(unused_imports,unused_variables,unreachable_code,dead_code,non_upper_case_globals)]
+#![allow(unused_imports,unused_variables,unreachable_code,dead_code,non_upper_case_globals,non_snake_case)]
 extern crate helper;
 use helper        	::*; // gets macros
 use helper::alias 	::*;
