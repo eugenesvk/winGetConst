1c142cbdaa83be023c6d27aa7ab3c494ae5cc5c2
eugenesvk <eugenesvk@users.noreply.github.com>
2023-10-09 01:14:23 +0700

    trustfall query: update imports

--- src/trustfall_rustdoc_old.rs
+++ src/trustfall_rustdoc_old.rs
@@ -1,3 +1,10 @@
+#![allow(unused_imports,unused_variables,unreachable_code,dead_code,non_upper_case_globals,non_snake_case)]
+extern crate helper;
+use std::path::PathBuf;
+use helper        	::*; // gets macros
+use helper::alias 	::*;
+use helper::helper	::*;
+
 use anyhow::Context;
 use std::collections::BTreeMap;
 use std::path::Path;
