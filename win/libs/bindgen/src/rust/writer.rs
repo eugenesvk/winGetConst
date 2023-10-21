3b97737a60ee2aa0c96d1de1aada24ef5178f7d0
eugenesvk <eugenesvk@users.noreply.github.com>
2023-10-21 23:48:38 +0700

    Revert "disable string escaping"
    This reverts commit aebcb1676348dda25bfc79587526272c3687df1e.

--- win/libs/bindgen/src/rust/writer.rs
+++ win/libs/bindgen/src/rust/writer.rs
@@ -429,8 +429,11 @@ impl<'a> Writer<'a> {
             Value::F64(value) => quote! { #value },
             Value::String(value) => {
                 let mut tokens = "".to_string(); // \"
-                for u in value.chars() {write!(tokens,"{}",u).unwrap();} //u.escape_default()
-                // String("\\Device\\FSVideo") → \Device\FSVideo
+
+                for u in value.chars() {
+                    write!(tokens, "{}", u.escape_default()).unwrap();
+                }
+
                 // tokens.push('\"');
                 tokens.into()
             }
